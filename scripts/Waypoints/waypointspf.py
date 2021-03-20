80210748d12242bc9f6803baefcc1509068e677f
IvanaColl <ivanacollado@gmail.com>
2021-03-20 13:00:09 -0600

    Broadcast desired path and removed desired speed

--- scripts/Waypoints/waypointspf.py
+++ scripts/Waypoints/waypointspf.py
@@ -11,11 +11,9 @@ class Test:
         self.testing = True
 
         self.path_pub = rospy.Publisher("/mission/waypoints", Float32MultiArray, queue_size=10)
-        self.d_speed_pub = rospy.Publisher("/guidance/desired_speed", Float64, queue_size=10)
 
-    def desired(self, path, speed):
+    def desired(self, path):
       self.path_pub.publish(path)
-      self.d_speed_pub.publish(speed)
 
 def main():
     rospy.init_node('waypoints', anonymous=False)
@@ -24,16 +22,13 @@ def main():
     path_array = Float32MultiArray()
     path_array.layout.data_offset = 5
     path_array.data = [0, 4, 45, 4, 0]
-    speed = Float64()
-    speed.data = 0.5
     while not rospy.is_shutdown() and t.testing:
         time.sleep(1)
         start_time = rospy.Time.now().secs
         while (rospy.Time.now().secs - start_time) <= 50 and not rospy.is_shutdown():
-            t.desired(path_array, speed)
+            t.desired(path_array)
             rate.sleep()
-        speed.data = 0.0
-        t.desired(path_array, speed)
+        t.desired(path_array)
         t.testing = False
         rospy.logwarn("Finished")
     rospy.spin()
--- scripts/Waypoints/waypointspfzz.py
+++ scripts/Waypoints/waypointspfzz.py
@@ -11,11 +11,9 @@ class Test:
         self.testing = True
 
         self.path_pub = rospy.Publisher("/mission/waypoints", Float32MultiArray, queue_size=10)
-        self.d_speed_pub = rospy.Publisher("/guidance/desired_speed", Float64, queue_size=10)
 
-    def desired(self, path, speed):
+    def desired(self, path):
       self.path_pub.publish(path)
-      self.d_speed_pub.publish(speed)
 
 def main():
     rospy.init_node('waypointspfzz', anonymous=False)
@@ -24,13 +22,11 @@ def main():
     path_array = Float32MultiArray()
     path_array.layout.data_offset = 9
     path_array.data = [2,2,6,-15,1,-25,6,-35, 0]
-    speed = Float64()
-    speed.data = 0.5
     while not rospy.is_shutdown() and t.testing:
         time.sleep(1)
         start_time = rospy.Time.now().secs
         while (rospy.Time.now().secs - start_time) <= 45 and not rospy.is_shutdown():
-            t.desired(path_array, speed)
+            t.desired(path_array)
             rate.sleep()
         t.testing = False
         rospy.logwarn("Finished")
