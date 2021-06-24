900b7c6ae3189506277f7e0212d487c6c304c157
alexglzg <alex_gg97@hotmail.com>
2021-06-23 20:44:54 -0500

    finite time strategies ready for testing

--- src/finite_time_asmc.cpp
+++ src/finite_time_asmc.cpp
@@ -24,6 +24,10 @@ public:
 
   int testing;
   int arduino;
+  int start;
+  double start_time;
+  double convergence_time_u;
+  double convergence_time_r;
 
   //Tracking variables
   float u_d;
@@ -44,8 +48,6 @@ public:
   float sigma_r_last;
   float sigma_r_dot;
   float sigma_r_abs;
-  float Delta_hat_dot_last_u;
-  float Delta_hat_dot_last_r;
   float sqr2;
   int convergence_u;
   int convergence_r;
@@ -56,28 +58,12 @@ public:
   float f_r;
   int sign_u;
   int sign_r;
-  int sign_delta_tilde_u;
-  int sign_delta_tilde_r;
-
-  float o_dot_dot;
-  float o_dot;
-  float o;
-  float o_last;
-  float o_dot_last;
-  float o_dot_dot_last;
-  static const float o1 = 2;
-  static const float o2 = 2;
-  static const float o3 = 2;
-
-  float g_dot_dot;
-  float g_dot;
-  float g;
-  float g_last;
-  float g_dot_last;
-  float g_dot_dot_last;
-  static const float g1 = 2;
-  static const float g2 = 2;
-  static const float g3 = 2;
+  float eta_u;
+  float eta_r;
+  int sign_u_sm;
+  int sign_r_sm;
+  float signvar_u;
+  float signvar_r;
 
   //Model pysical parameters
   float Xu;
@@ -110,19 +96,16 @@ public:
   float mu_r;
   float lambda_u;
   float lambda_r;
-  float eta_u;
-  float eta_r;
   float k_u;
   float k_r;
   float Kmin_u;
   float Kmin_r;
 
   //Fixed-time variables
-  float L1;
-  float L1_dot;
-  float epsilon;
-  float ro;
-
+  float L1_u;
+  float L1_r;
+  float t_c;
+ 
   //Observer
   float Delta_hat_u;
   float Delta_hat_r;
@@ -146,8 +129,6 @@ public:
     heading_gain_pub = n.advertise<std_msgs::Float64>("/usv_control/asmc/heading_gain", 1000);
     heading_error_pub = n.advertise<std_msgs::Float64>("/usv_control/controller/heading_error", 1000);
     control_input_pub = n.advertise<geometry_msgs::Pose2D>("/usv_control/controller/control_input", 1000);
-    observer_pub = n.advertise<geometry_msgs::Pose2D>("/usv_control/observer", 1000);
-    observer_error_pub = n.advertise<geometry_msgs::Pose2D>("/usv_control/observer_error", 1000);
     
     //ROS Subscribers
     desired_speed_sub = n.subscribe("/guidance/desired_speed", 1000, &AdaptiveSlidingModeControl::desiredSpeedCallback, this);
@@ -155,48 +136,42 @@ public:
     local_vel_sub = n.subscribe("/vectornav/ins_2d/local_vel", 1000, &AdaptiveSlidingModeControl::velocityCallback, this);
     flag_sub = n.subscribe("/arduino_br/ardumotors/flag", 1000, &AdaptiveSlidingModeControl::flagCallback, this);
     ardu_sub = n.subscribe("arduino", 1000, &AdaptiveSlidingModeControl::arduinoCallback, this);
+    start_sub = n.subscribe("/start_prescribed", 1000, &AdaptiveSlidingModeControl::startCallback, this);
 
-    static const float dL1 = 0.1;
-    static const float dL1_dot = 0;
-    static const float depsilon = 0.001;
-    static const float dro = 0.1;
-    static const float dK2_u = 0.1;
-    static const float dK2_r = 0.1;
-    static const float dmu_u = 0.05;
-    static const float dmu_r = 0.05;
-    static const float dlambda_u = 0.01;
-    static const float dlambda_r = 0.01;
-    static const float deta_u = 0.4714;
-    static const float deta_r = 0.1851;
-    static const float dk_u = 0.1;
-    static const float dk_r = 0.1;
-    static const float dKmin_u = 0.05;
-    static const float dKmin_r = 0.05;
+    static const float dL1_u = 0.15;
+    static const float dL1_r = 0.15;
+    static const float dK2_u = 0.01;
+    static const float dK2_r = 0.01;
+    static const float dmu_u = 0.1;
+    static const float dmu_r = 0.1;
+    static const float dlambda_u = 0.001;
+    static const float dlambda_r = 0.001;
+    static const float dt_c = 3;
+    static const float dk_u = 0.01;
+    static const float dk_r = 0.02;
+    static const float dKmin_u = 0.01;
+    static const float dKmin_r = 0.01;
 
-    n.param("/fixed_time_asmc/L1", L1, dL1);
-    n.param("/fixed_time_asmc/L1_dot", L1_dot, dL1_dot);
-    n.param("/fixed_time_asmc/epsilon", epsilon, depsilon);
-    n.param("/fixed_time_asmc/ro", ro, dro);
-    n.param("/fixed_time_asmc/K2_u", K2_u, dK2_u);
-    n.param("/fixed_time_asmc/K2_r", K2_r, dK2_r);
-    n.param("/fixed_time_asmc/mu_u", mu_u, dmu_u);
-    n.param("/fixed_time_asmc/mu_r", mu_r, dmu_r);
-    n.param("/fixed_time_asmc/lambda_u", lambda_u, dlambda_u);
-    n.param("/fixed_time_asmc/lambda_r", lambda_r, dlambda_r);
-    n.param("/fixed_time_asmc/eta_u", eta_u, deta_u);
-    n.param("/fixed_time_asmc/eta_r", eta_r, deta_r);
-    n.param("/fixed_time_asmc/k_u", k_u, dk_u);
-    n.param("/fixed_time_asmc/k_r", k_r, dk_r);
-    n.param("/fixed_time_asmc/Kmin_u", Kmin_u, dKmin_u);
-    n.param("/fixed_time_asmc/Kmin_r", Kmin_r, dKmin_r);
+    n.param("/finite_time_asmc/L1_u", L1_u, dL1_u);
+    n.param("/finite_time_asmc/L1_r", L1_r, dL1_r);
+    n.param("/finite_time_asmc/K2_u", K2_u, dK2_u);
+    n.param("/finite_time_asmc/K2_r", K2_r, dK2_r);
+    n.param("/finite_time_asmc/mu_u", mu_u, dmu_u);
+    n.param("/finite_time_asmc/mu_r", mu_r, dmu_r);
+    n.param("/finite_time_asmc/lambda_u", lambda_u, dlambda_u);
+    n.param("/finite_time_asmc/lambda_r", lambda_r, dlambda_r);
+    n.param("/finite_time_asmc/t_c", t_c, dt_c);
+    n.param("/finite_time_asmc/k_u", k_u, dk_u);
+    n.param("/finite_time_asmc/k_r", k_r, dk_r);
+    n.param("/finite_time_asmc/Kmin_u", Kmin_u, dKmin_u);
+    n.param("/finite_time_asmc/Kmin_r", Kmin_r, dKmin_r);
 
     u_d = 0;
     r_d = 0;
     testing = 0;
     arduino = 0;
-    sqr2 = pow(2,0.5);
-    Delta_hat_u = 0;
-    Delta_hat_r = 0;
+    start = 0;
+    sqr2 = std::pow(2,0.5);
     convergence_u = 0;
     convergence_r = 0;
 
@@ -229,24 +204,29 @@ public:
     arduino = _ardu -> data;
   }
 
+  void startCallback(const std_msgs::UInt8::ConstPtr& _start)
+  {
+    start = _start -> data;
+  }
+
   void control()
   {
     if (testing == 1 && arduino == 1){
       Xu = -25;
       Xuu = 0;
-      u_abs = std::abs(u);
+      u_abs = std::fabs(u);
       if (u_abs > 1.2){
         Xu = 64.55;
         Xuu = -70.92;
       }
 
-      Nr = (-0.52)*pow(pow(u,2) + pow(v,2),0.5);
+      Nr = (-0.52)*std::pow(std::pow(u,2) + std::pow(v,2),0.5);
 
       g_u = (1 / (m - X_u_dot));
       g_r = (1 / (Iz - N_r_dot));
 
       f_u = (((m - Y_v_dot)*v*r + (Xuu*u_abs*u + Xu*u)) / (m - X_u_dot));
-      f_r = (((-X_u_dot + Y_v_dot)*u*v + (Nr*r + Nrr*r*std::abs(r))) / (Iz - N_r_dot));
+      f_r = (((-X_u_dot + Y_v_dot)*u*v + (Nr*r + Nrr*r*std::fabs(r))) / (Iz - N_r_dot));
 
       e_u = u_d - u;
       e_r = r_d - r;
@@ -260,65 +240,43 @@ public:
       sigma_u = e_u + lambda_u * e_u_int;
       sigma_r = e_r + lambda_r * e_r_int;
 
-      sigma_u_dot = (sigma_u - sigma_u_last) / integral_step;
+      sigma_u_abs = std::fabs(sigma_u);
+      sigma_r_abs = std::fabs(sigma_r);
 
-      o_dot_dot = (((sigma_u_dot - o_last) * o1) - (o3 * o_dot_last)) * o2;
-      o_dot = (integral_step)*(o_dot_dot + o_dot_dot_last)/2 + o_dot;
-      o = (integral_step)*(o_dot + o_dot_last)/2 + o;
-      sigma_u_dot = o;
-      o_last = o;
-      o_dot_last = o_dot;
-      o_dot_dot_last = o_dot_dot;
-      sigma_u_last = sigma_u;
-
-      sigma_r_dot = (sigma_r - sigma_r_last) / integral_step;
-
-      g_dot_dot = (((sigma_r_dot - g_last) * g1) - (g3 * g_dot_last)) * g2;
-      g_dot = (integral_step)*(g_dot_dot + g_dot_dot_last)/2 + g_dot;
-      g = (integral_step)*(g_dot + g_dot_last)/2 + g;
-      sigma_r_dot = g;
-      g_last = g;
-      g_dot_last = g_dot;
-      g_dot_dot_last = g_dot_dot;
-      sigma_r_last = sigma_r;
+      if (start == 1){
+        eta_u = sqr2*sigma_u_abs / t_c;
+        eta_r = sqr2*sigma_r_abs / t_c;
+        ROS_FATAL_STREAM("eta_u = " << eta_u);
+        ROS_FATAL_STREAM("eta_r = " << eta_r);
+        start_time = ros::Time::now().toSec();
+      }
       
-      sigma_u_abs = std::abs(sigma_u);
-      sigma_r_abs = std::abs(sigma_r);
-      
-      if (sigma_u == 0){
-        sign_u = 0;
-      }
-      else {
-        sign_u = copysign(1,sigma_u);
-      }
-
-      if (sigma_r == 0){
-        sign_r = 0;
-      }
-      else {
-        sign_r = copysign(1,sigma_r);
-      }
-
-      if (sigma_u_abs <= mu_u){
+      if (((sigma_u_abs <= mu_u) || (ros::Time::now().toSec() - start_time >= t_c)) && ((convergence_u == 0)  && (start > 0))){
         convergence_u = 1;
+        convergence_time_u = ros::Time::now().toSec() - start_time;
+        ROS_FATAL_STREAM("convergence time u = " << convergence_time_u);
       }
 
-      if (sigma_r_abs <= mu_r){
+      if (((sigma_r_abs <= mu_r) || (ros::Time::now().toSec() - start_time >= t_c)) && ((convergence_r == 0)  && (start > 0))){
         convergence_r = 1;
+        convergence_time_r = ros::Time::now().toSec() - start_time;
+        ROS_FATAL_STREAM("convergence time r = " << convergence_time_r);
       }
 
       if (convergence_u == 0){
-        Ka_u = (1/pow(sigma_u_abs,0.5)) * (Delta_hat_u*sign_u + (eta_u/sqr2) - K2_u*sigma_u_abs);
+        Ka_u = std::max((1/std::pow(sigma_u_abs,0.5)) * (L1_u + (eta_u/sqr2) - K2_u*sigma_u_abs),0.0);
+            if (start == 1){
+              ROS_FATAL_STREAM("Ka_u = " << Ka_u);
+            }
       }
       else if (convergence_u == 1){
-        int sign_u_sm = 0;
         if (Ka_u > Kmin_u){
-          float signvar = sigma_u_abs - mu_u;
-          if (signvar == 0){
+          signvar_u = sigma_u_abs - mu_u;
+          if (signvar_u == 0){
             sign_u_sm = 0;
           }
           else {
-            sign_u_sm = copysign(1,signvar);
+            sign_u_sm = copysignf(1,signvar_u);
           }
           Ka_dot_u = k_u * sign_u_sm;
         }
@@ -330,17 +288,19 @@ public:
       }
 
       if (convergence_r == 0){
-        Ka_r = (1/pow(sigma_r_abs,0.5)) * (Delta_hat_r*sign_r + (eta_r/sqr2) - (K2_r*sigma_r_abs));
+        Ka_r = std::max((1/std::pow(sigma_r_abs,0.5)) * (L1_r + (eta_r/sqr2) - K2_r*sigma_r_abs),0.0);
+            if (start == 1){
+              ROS_FATAL_STREAM("Ka_r = " << Ka_r);
+            }
       }
       else if (convergence_r == 1){
-        int sign_r_sm = 0;
         if (Ka_r > Kmin_r){
-          float signvar = sigma_r_abs - mu_r;
-          if (signvar == 0){
+          signvar_r = sigma_r_abs - mu_r;
+          if (signvar_r == 0){
             sign_r_sm = 0;
           }
           else {
-            sign_r_sm = copysign(1,signvar);
+            sign_r_sm = copysignf(1,signvar_r);
           }
           Ka_dot_r = k_r * sign_r_sm;
         }
@@ -351,54 +311,25 @@ public:
         Ka_dot_last_r = Ka_dot_r;
       }
 
-      ua_u = ((-Ka_u) * pow(sigma_u_abs,0.5) * sign_u) - (K2_u*sigma_u);
-      ua_r = ((-Ka_r) * pow(sigma_r_abs,0.5) * sign_r) - (K2_r*sigma_r);
-
-      Delta_tilde_u = sigma_u_dot - Delta_hat_u - ua_u;
-      Delta_tilde_r = sigma_r_dot - Delta_hat_r - ua_r;
-
-      sign_delta_tilde_u = 0;
-      sign_delta_tilde_r = 0;
-
-      if (Delta_tilde_u == 0){
-        sign_delta_tilde_u = 0;
+      if (sigma_u == 0){
+        sign_u = 0;
       }
       else {
-        sign_delta_tilde_u = copysign(1,Delta_tilde_u);
+        sign_u = copysignf(1,sigma_u);
       }
 
-      if (Delta_tilde_r == 0){
-        sign_delta_tilde_r = 0;
+      if (sigma_r == 0){
+        sign_r = 0;
       }
       else {
-        sign_delta_tilde_r = copysign(1,Delta_tilde_r);
+        sign_r = copysignf(1,sigma_r);
       }
 
-      if (std::abs(Delta_tilde_u) < epsilon){
-        k_delta_u = L1_dot / pow(std::abs(epsilon),0.5);
-      }
-      else{
-        k_delta_u = (1 / pow(std::abs(Delta_tilde_u),0.5)) * ((L1 * eta_u)/(ro*std::abs(u_d)*sqr2) + L1_dot);
-      }
-
-      if (std::abs(Delta_tilde_r) < epsilon){
-        k_delta_r = L1_dot / pow(std::abs(epsilon),0.5);
-      }
-      else{
-        k_delta_r = (1 / pow(std::abs(Delta_tilde_r),0.5)) * ((L1 * eta_r)/(ro*std::abs(r_d)*sqr2) + L1_dot);
-      }
-
-      Delta_hat_dot_u = k_delta_u * pow(std::abs(Delta_tilde_u),0.5) * sign_delta_tilde_u;
-      Delta_hat_dot_r = k_delta_r * pow(std::abs(Delta_tilde_r),0.5) * sign_delta_tilde_r;
-
-      Delta_hat_u = (integral_step)*(Delta_hat_dot_u + Delta_hat_dot_last_u)/2 + Delta_hat_u;
-      Delta_hat_dot_last_u = Delta_hat_dot_u;
-
-      Delta_hat_r = (integral_step)*(Delta_hat_dot_r + Delta_hat_dot_last_r)/2 + Delta_hat_r;
-      Delta_hat_dot_last_r = Delta_hat_dot_r;
+      ua_u = ((-Ka_u) * std::pow(sigma_u_abs,0.5) * sign_u) - (K2_u*sigma_u);
+      ua_r = ((-Ka_r) * std::pow(sigma_r_abs,0.5) * sign_r) - (K2_r*sigma_r);
 
       Tx = ((lambda_u * e_u) - f_u - ua_u) / g_u; //surge force
-      Tz = ((lambda_r * e_r) - f_r - ua_r) / g_r; //yaw rate moment
+      Tz = ((lambda_r * e_r) - f_r - ua_r) / g_r; //yaw moment
       
       if (Tx > 73){
         Tx = 73;
@@ -417,9 +348,9 @@ public:
         Tx = 0;
         Tz = 0;
 
-        Ka_u = 0;
+        Ka_u = Kmin_u;
         Ka_dot_last_u = 0;
-        Ka_r = 0;
+        Ka_r = Kmin_r;
         Ka_dot_last_r = 0;
         
         e_u = 0;
@@ -433,28 +364,11 @@ public:
         sigma_u_last = 0;
         sigma_r = 0;
         sigma_r_last = 0;
-        
-        Delta_hat_u = 0;
-        Delta_hat_r = 0;
-        Delta_hat_dot_last_u = 0;
-        Delta_hat_dot_last_r = 0;
-        
-        o_dot_dot = 0;
-        o_dot = 0;
-        o = 0;
-        o_last = 0;
-        o_dot_last = 0;
-        o_dot_dot_last = 0;
-        
-        g_dot_dot = 0;
-        g_dot = 0;
-        g = 0;
-        g_last = 0;
-        g_dot_last = 0;
-        g_dot_dot_last = 0;
-        
+            
         convergence_u = 0;
         convergence_r = 0;
+        eta_u = 0;
+        eta_r = 0;
       }
 
       port_t = (Tx / 2) + (Tz / B);
@@ -505,12 +419,6 @@ public:
       ctrl_input.x = Tx;
       ctrl_input.theta = Tz;
 
-      observer.x = Delta_hat_u;
-      observer.theta = Delta_hat_r;
-
-      observer_error.x = Delta_tilde_u;
-      observer_error.theta = Delta_tilde_r;
-
       right_thruster_pub.publish(rt);
       left_thruster_pub.publish(lt);
 
@@ -521,8 +429,6 @@ public:
       heading_error_pub.publish(er);
       heading_sigma_pub.publish(sp);
       control_input_pub.publish(ctrl_input);
-      observer_pub.publish(observer);
-      observer_error_pub.publish(observer_error);
     }
   }
 
@@ -538,20 +444,19 @@ private:
   ros::Publisher heading_gain_pub;
   ros::Publisher heading_error_pub;
   ros::Publisher control_input_pub;
-  ros::Publisher observer_pub;
-  ros::Publisher observer_error_pub;
 
   ros::Subscriber desired_speed_sub;
   ros::Subscriber desired_heading_sub;
   ros::Subscriber local_vel_sub;
   ros::Subscriber flag_sub;
   ros::Subscriber ardu_sub;
+  ros::Subscriber start_sub;
 };
 
 // Main
 int main(int argc, char *argv[])
 {
-  ros::init(argc, argv, "fixed_time_asmc");
+  ros::init(argc, argv, "finite_time_asmc");
   AdaptiveSlidingModeControl adaptiveSlidingModeControl;
   int rate = 100;
   ros::Rate loop_rate(rate);
